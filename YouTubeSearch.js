1fc7008211dbb35738427dacaef114a1cb96a209
TheNovaMist <96658153+TheNovaMist@users.noreply.github.com>
2022-01-28 15:19:03 +0800

    First commit.

---
+++ YouTubeSearch.js
@@ -0,0 +1,8 @@
+javascript: (function () {
+    /* rotate_IMG.2021.09.14 */
+    var dir_ = 'https://www.youtube.com/results?search_query=';
+    var selObj = window.getSelection();
+    var url = dir_ + selObj;
+    window.open(
+        url, "_blank");
+})()
